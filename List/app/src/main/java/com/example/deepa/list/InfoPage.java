f244425fa953681088c1212056693f8b078da738
Deepa <dhc5cu@virginia.edu>
2016-02-13 22:49:36 -0500

    Still not done

--- List/app/src/main/java/com/example/deepa/list/InfoPage.java
+++ List/app/src/main/java/com/example/deepa/list/InfoPage.java
@@ -4,8 +4,11 @@ import android.support.design.widget.AppBarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.support.v7.widget.ActionBarOverlayLayout;
+import android.widget.Toast;
 
 
 public class InfoPage extends AppCompatActivity {
@@ -22,6 +25,7 @@ public class InfoPage extends AppCompatActivity {
         String info = ListData.getInfoList().get(index);
         boolean completed = ListData.getCompletedList().get(index);
 
+
         // Update title label
         TextView titleView = (TextView) findViewById(R.id.list_item_name);
         titleView.setText(title);
@@ -30,10 +34,35 @@ public class InfoPage extends AppCompatActivity {
         TextView infoView = (TextView) findViewById(R.id.list_item_info);
         infoView.setText(info);
 
-
+        Button b = (Button) findViewById(R.id.the_button);
+        if(completed) {
+           b.setText("Done!");
+        }
+        else {
+            b.setText("Complete Me!");
+        }
+        /*
+        View.OnClickListener buttonListener = new View.OnClickListener() {
+            boolean clicked = false;
+            Intent intent = getIntent();
+            int index = intent.getIntExtra(MainActivity.INDEX_MESSAGE, 99);
+            boolean completed = ListData.getCompletedList().get(index);
+            @Override
+            public void onClick(View v) {
+                if(completed) {
+                    completed = !completed;
+                    ListData.getCompletedList().set(index, completed);
+                }
+                else {
+                    completed = !completed;
+                    ListData.getCompletedList().set(index, completed);
+                }
+            }
+        };
+        b.setOnClickListener(buttonListener);
+        */
  //       RelativeLayout layout = (RelativeLayout) findViewById(R.id.detail_layout);
  //       layout.addView(textView);
-
 
     }
 
